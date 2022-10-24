cb3115e1141e64b0bce2a31a263d8c9198c37dec
Angus <angus.webb197@gmail.com>
2022-10-25 10:52:50 +1300

    Created calc buttons and got the calculator working

--- src/index.js
+++ src/index.js
@@ -3,38 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { useState} from "react";
+
 
 
 function app() {
-
-  const [answer, setAnswer] = useState("");
-  const [first, setFirst] = useState(10);
-  const [second, setSecond] = useState(22);
-}
-const Calculate = (value) => {
-  console.log("value", value);
-  if (value !== ""){
-    switch (value) {
-  
-  case "-":
-    setAnswer(first - second);
-    break;
-    case "+":
-    setAnswer(first + second);
-    break;
-    case "X":
-      setAnswer(first * second);
-      break;
-      case "/":
-        setAnswer(first / second);
-        break;
-  
-        default:
-          setAnswer("error");
-          break;
-    }
-  }
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
